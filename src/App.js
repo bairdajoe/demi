6fabda994a8ce252f8e648b793f39ea193b7396d
bairdajoe <bairdajoe@gmail.com>
2025-05-21 17:08:47 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Demi Dungeons</h1>
+      <p>This is a working live deployment of your game base.</p>
+    </div>
+  );
+}
+
+export default App;
